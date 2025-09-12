0e234af122a7f9d88ba6ab8d70383a97a80a50bb
Supriya Kaity <112101324+SupriyaKaity@users.noreply.github.com>
2025-09-12 15:06:09 +0000

    hooks and project

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+import Chai from './Chai'
+
+
+function App() {
+  return (
+    <>
+    <Chai/>
+    <h1>Hello World | Supriya</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,17 @@
+import Chai from "./chai"
+
+
+function App() {
+  const username = "chai aur code"
+
+  return (
+   //<h1>Hello World with vite | Supriya</h1>
+   <>
+   <Chai/>
+   <h1>chai aur react {username}</h1>
+   <p>test para</p>
+   </>
+  )
+}
+
+export default App
